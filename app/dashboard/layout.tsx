806e093e8eecc0928e4671b90838db4666445487
jffhnry <dzkjeffhenry97@gmail.com>
2025-06-30 14:21:29 +0700

    Initial commit

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,14 @@
+import SideNav from '@/app/ui/dashboard/sidenav';
+import { inter } from '@/app/ui/fonts';
+ 
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
